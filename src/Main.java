aaba969a6c86383ae6ac09f10191c2ba50a05823
faso357 <francois1357@gmail.com>
2023-03-29 10:09:59 -0500

    issue in sun still resolving

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ SolarSystem solarSystem = new SolarSystem();
 solarSystem.plantes="2";
 solarSystem.moon="2";
 solarSystem.sun="2";
+Sun sun = new Sun();
+sun.color="red"
+    sun.temperature= 785785867;
+sun.heat="jhvhjfjhc";
+
     }
 }
